f2ec88117d5cce9036764d51398d3eda2c7150ba
Sergey Shulika <shulika619@gmail.com>
2024-01-04 18:34:52 +0300

    add model

---
+++ src/main/java/dev/shulika/restapiexample/model/Comment.java
@@ -0,0 +1,24 @@
+package dev.shulika.restapiexample.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "text")
+    private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "task_id")
+    private Task task;
+
+}
