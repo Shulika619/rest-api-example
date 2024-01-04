8ad701133ffc16624b8ea642d2d077df51e091aa
Sergey Shulika <shulika619@gmail.com>
2024-01-05 02:53:19 +0300

    add NotFoundException

---
+++ src/main/java/dev/shulika/restapiexample/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package dev.shulika.restapiexample.exception;
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
