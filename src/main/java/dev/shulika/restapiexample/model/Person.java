b3e62d6946ef6f05f53ff9f8b377b4ca6e6f0b3b
Sergey Shulika <shulika619@gmail.com>
2024-01-04 18:35:29 +0300

    add unique to email

--- src/main/java/dev/shulika/restapiexample/model/Person.java
+++ src/main/java/dev/shulika/restapiexample/model/Person.java
@@ -20,7 +20,7 @@ public class Person {
     @Column(name = "last_name")
     private String lastName;
 
-    @Column(name = "email")
+    @Column(name = "email", unique = true)
     private String email;
 
     @Column(name = "password")
