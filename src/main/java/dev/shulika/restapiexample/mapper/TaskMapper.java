40fbb2e54fa78cf4108d6bf4d610c2ea385780e5
Sergey Shulika <shulika619@gmail.com>
2024-01-06 02:01:21 +0300

    edit TaskMapper

--- src/main/java/dev/shulika/restapiexample/mapper/TaskMapper.java
+++ src/main/java/dev/shulika/restapiexample/mapper/TaskMapper.java
@@ -4,12 +4,21 @@ import dev.shulika.restapiexample.dto.TaskDto;
 import dev.shulika.restapiexample.model.Task;
 import org.mapstruct.*;
 
+
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface TaskMapper {
+
+    @Mapping(source = "authorId", target = "author.id")
+    @Mapping(source = "executorId", target = "executor.id")
     Task toEntity(TaskDto taskDto);
 
+    @Mapping(source = "author.id", target = "authorId")
+    @Mapping(source = "executor.id", target = "executorId")
     TaskDto toDto(Task task);
 
+    @Mapping(source = "authorId", target = "author.id")
+    @Mapping(source = "executorId", target = "executor.id")
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     Task partialUpdate(TaskDto taskDto, @MappingTarget Task task);
+
 }
