8ae5297ad11a618edf8a14c0540c2bf31e22970d
Sergey Shulika <shulika619@gmail.com>
2024-01-06 02:03:03 +0300

    add TaskController

---
+++ src/main/java/dev/shulika/restapiexample/controller/TaskController.java
@@ -0,0 +1,50 @@
+package dev.shulika.restapiexample.controller;
+
+import dev.shulika.restapiexample.dto.TaskDto;
+import dev.shulika.restapiexample.model.Task;
+import dev.shulika.restapiexample.service.TaskService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/tasks")
+public class TaskController {
+
+    private final TaskService taskService;
+
+    @GetMapping
+    public Page<TaskDto> getAll(Pageable pageable) {
+        return taskService.findAll(pageable);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TaskDto> getById(@Valid @PathVariable Long id) {
+        TaskDto taskDto = taskService.findById(id);
+        return new ResponseEntity<>(taskDto, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Task> create(@Valid @RequestBody TaskDto taskDto) {
+        Task task = taskService.create(taskDto);
+        return new ResponseEntity<>(task, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> update(@Valid @PathVariable Long id, @Valid @RequestBody TaskDto taskDto) {
+        Task task = taskService.updateById(id, taskDto);
+        return new ResponseEntity<>(task, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@Valid @PathVariable Long id) {
+        taskService.deleteById(id);
+        return new ResponseEntity<>("DELETED", HttpStatus.OK);
+    }
+
+}
