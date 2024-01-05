bc5a4fba86f1c8112b70598c6a13d87b4d5a74a2
Sergey Shulika <shulika619@gmail.com>
2024-01-06 02:40:27 +0300

    add CommentMapper

--- src/main/java/dev/shulika/restapiexample/mapper/CommentMapper.java
+++ src/main/java/dev/shulika/restapiexample/mapper/CommentMapper.java
@@ -6,10 +6,14 @@ import org.mapstruct.*;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface CommentMapper {
+
+    @Mapping(source = "taskId", target = "task.id")
     Comment toEntity(CommentDto commentDto);
 
+    @Mapping(source = "task.id", target = "taskId")
     CommentDto toDto(Comment comment);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     Comment partialUpdate(CommentDto commentDto, @MappingTarget Comment comment);
+
 }
