a249930850f83592a9d8f402d85b856d04f5877f
Sergey Shulika <shulika619@gmail.com>
2024-01-05 15:25:39 +0300

    add method to PersonRepository

--- src/main/java/dev/shulika/restapiexample/repository/PersonRepository.java
+++ src/main/java/dev/shulika/restapiexample/repository/PersonRepository.java
@@ -4,4 +4,5 @@ import dev.shulika.restapiexample.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
+    boolean existsByEmailAllIgnoreCase(String email);
 }
