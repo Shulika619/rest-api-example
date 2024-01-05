651abb01ca48931f3fd07004142b1fd3a28ea9b1
Sergey Shulika <shulika619@gmail.com>
2024-01-06 02:05:13 +0300

    edit CommandDto change @NotBlank to @NotNull

--- src/main/java/dev/shulika/restapiexample/dto/CommentDto.java
+++ src/main/java/dev/shulika/restapiexample/dto/CommentDto.java
@@ -1,8 +1,12 @@
 package dev.shulika.restapiexample.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 
 import java.io.Serializable;
@@ -17,7 +21,7 @@ public class CommentDto implements Serializable {
     @Length(min = 1, max = 1024, message = "Text must be from 1 to 1024")
     String text;
 
-    @NotBlank(message = "Task id is mandatory")
+    @NotNull(message = "Task id is mandatory")
     @Positive(message = "Task id must be positive Long")
     Long taskId;
 
