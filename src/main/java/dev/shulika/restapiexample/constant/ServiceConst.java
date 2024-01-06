e8eae5cf946cc104d917b4d255857b9a5ac8794d
Sergey Shulika <shulika619@gmail.com>
2024-01-06 21:24:01 +0300

    add ServiceConst

---
+++ src/main/java/dev/shulika/restapiexample/constant/ServiceConst.java
@@ -0,0 +1,10 @@
+package dev.shulika.restapiexample.constant;
+
+public interface ServiceConst {
+
+    String PERSON_EXIST = "Person already exist! email: ";
+    String PERSON_NOT_FOUND = "Person not found! id: ";
+    String COMMENT_NOT_FOUND = "Comment not found! id: ";
+    String TASK_NOT_FOUND = "Task not found! id: ";
+
+}
