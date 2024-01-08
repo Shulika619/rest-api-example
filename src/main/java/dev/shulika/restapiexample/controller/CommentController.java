5dd6e0a7dc3f1c86954be7a3b7225eb92d9bd1aa
Sergey Shulika <shulika619@gmail.com>
2024-01-09 01:41:14 +0300

    add openApi description for controllers

--- src/main/java/dev/shulika/restapiexample/controller/CommentController.java
+++ src/main/java/dev/shulika/restapiexample/controller/CommentController.java
@@ -3,6 +3,8 @@ package dev.shulika.restapiexample.controller;
 import dev.shulika.restapiexample.dto.comment.CommentRequestDto;
 import dev.shulika.restapiexample.dto.comment.CommentResponseDto;
 import dev.shulika.restapiexample.service.CommentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -14,38 +16,49 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/comments")
+@Tag(name = "Comment", description = "Comment management APIs")
 public class CommentController {
 
     private final CommentService commentService;
 
+    @Operation(summary = "Get comments", description = "Provides all comments, supports pagination and filtering")
     @GetMapping
     public Page<CommentResponseDto> getAll(Pageable pageable) {
         return commentService.findAll(pageable);
     }
 
+    @Operation(summary = "Get a comment", description = "Provides comment by id")
     @GetMapping("/{id}")
     public ResponseEntity<CommentResponseDto> getById(@Valid @PathVariable Long id) {
         CommentResponseDto commentResponseDto = commentService.findById(id);
         return new ResponseEntity<>(commentResponseDto, HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Get comments by task id",
+            description = "Provides all comments with the specified task id, supports pagination and filtering")
     @GetMapping("/tasks/{taskId}")
     public Page<CommentResponseDto> getById(@Valid @PathVariable Long taskId, Pageable pageable) {
         return commentService.findByTaskId(taskId, pageable);
     }
 
+    @Operation(summary = "Create a comment", description = "Allows you to create a comment")
     @PostMapping
     public ResponseEntity<CommentResponseDto> create(@Valid @RequestBody CommentRequestDto commentRequestDto) {
         CommentResponseDto commentResponseDto = commentService.create(commentRequestDto);
         return new ResponseEntity<>(commentResponseDto, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Update a comment", description = "Allows you to update a comment by its id")
     @PutMapping("/{id}")
-    public ResponseEntity<CommentResponseDto> update(@Valid @PathVariable Long id, @Valid @RequestBody CommentRequestDto commentRequestDto) {
+    public ResponseEntity<CommentResponseDto> update(
+            @Valid @PathVariable Long id,
+            @Valid @RequestBody CommentRequestDto commentRequestDto) {
         CommentResponseDto commentResponseDto = commentService.updateById(id, commentRequestDto);
         return new ResponseEntity<>(commentResponseDto, HttpStatus.OK);
     }
 
+    @Operation(summary = "Delete a comment", description = "Allows you to delete a comment by its id")
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@Valid @PathVariable Long id) {
