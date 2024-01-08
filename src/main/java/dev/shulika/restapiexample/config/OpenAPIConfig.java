9a4fc2d6a8c9fd9c40d303c774973a2e36010f22
Sergey Shulika <shulika619@gmail.com>
2024-01-08 23:43:53 +0300

    rename SwaggerConfig to OpenAPIConfig and add data

--- src/main/java/dev/shulika/restapiexample/config/OpenAPIConfig.java
+++ src/main/java/dev/shulika/restapiexample/config/OpenAPIConfig.java
@@ -1,6 +1,7 @@
 package dev.shulika.restapiexample.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
@@ -15,7 +16,13 @@ public class OpenAPIConfig {
     public OpenAPI api() {
         return new OpenAPI()
                 .servers(List.of(new Server().url("http://localhost:8080")))
-                .info(new Info().title("Task Management System API").version("0.0.1"));
+                .info(new Info()
+                        .title("Task Management System API")
+                        .version("0.0.1")
+                        .description("Simple REST API example")
+                        .contact(new Contact().email("shulika619@gmail.com").name("Sergey Shulika").url("https://github.com/Shulika619"))
+
+                );
     }
 
 }
