78156df2b44adae3a83d0e0a802e0e02daedc130
Sergey Shulika <shulika619@gmail.com>
2024-01-08 22:31:24 +0300

    rename SwaggerConfig to OpenAPIConfig

--- src/main/java/dev/shulika/restapiexample/config/OpenAPIConfig.java
+++ src/main/java/dev/shulika/restapiexample/config/OpenAPIConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import java.util.List;
 
 @Configuration
-public class SwaggerConfig {
+public class OpenAPIConfig {
 
     @Bean
     public OpenAPI api() {
