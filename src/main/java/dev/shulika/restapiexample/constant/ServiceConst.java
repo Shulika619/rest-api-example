a19fa8eb9997a88bb551766f565cee7e06c6e4f8
Sergey Shulika <shulika619@gmail.com>
2024-01-11 00:34:41 +0300

    add constants

--- src/main/java/dev/shulika/restapiexample/constant/ServiceConst.java
+++ src/main/java/dev/shulika/restapiexample/constant/ServiceConst.java
@@ -4,6 +4,8 @@ public interface ServiceConst {
 
     String BAD_CREDENTIALS = "Authentication Failure: Bad Credentials, check email/password";
     String ACCESS_DENIED = "Authorization Failure: Access denied, you dont have permission to access this resource";
+    String SIGNATURE_JWT = "Authorization Failure: JWT signature is not valid";
+    String EXPIRED_JWT = "Authorization Failure: JWT already expired";
     String PERSON_EXIST = "Person already exist! email: ";
     String PERSON_NOT_FOUND = "Person not found! id: ";
     String PERSON_NOT_FOUND_EMAIL = "Person not found! email: ";
