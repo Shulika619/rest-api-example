192c7eb79000c19b8df81a62ed6d864ffda162c6
Sergey Shulika <shulika619@gmail.com>
2024-01-10 20:38:36 +0300

    add const

--- src/main/java/dev/shulika/restapiexample/constant/ServiceConst.java
+++ src/main/java/dev/shulika/restapiexample/constant/ServiceConst.java
@@ -4,6 +4,8 @@ public interface ServiceConst {
 
     String PERSON_EXIST = "Person already exist! email: ";
     String PERSON_NOT_FOUND = "Person not found! id: ";
+    String PERSON_NOT_FOUND_EMAIL = "Person not found! email: ";
+    String INVALID_EMAIL_PASSWORD = "Invalid email or password";
     String COMMENT_NOT_FOUND = "Comment not found! id: ";
     String TASK_NOT_FOUND = "Task not found! id: ";
 
